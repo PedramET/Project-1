b26c43b71a5b238969ee529cb5ec6df73c593ae2
PedramET <arashetminan6@gmail.com>
2025-10-29 22:05:43 +0300

    Update GroupProject1-Calculator.py

--- GroupProject1-Calculator.py
+++ GroupProject1-Calculator.py
@@ -1,3 +1,21 @@
 # Calculator
 
-print("Hello")
+
+import math
+
+math_operation = {
+    "^":    math.pow,
+    "sqrt": math.sqrt,
+    "sin":  math.sin,
+    "cos":  math.cos,
+    "tan":  math.tan,
+}
+
+def evaluate(input):
+    try:
+        result = eval(input, math_operation)
+        return result
+    except Exception as e:
+        return f"Error: {e}"
+    
+
